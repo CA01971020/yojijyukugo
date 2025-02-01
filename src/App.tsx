c10945791ff95eecb47d8c71c30c7ba489322a85
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-02-02 02:59:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./index";
 import { kanji } from "./data/kanji";
+import Button from "./button";
+import Menu from "./menu";
 
-function randomnum() {
+export function randomnum() {
   const maxnum = kanji.length;
   let count = 0;
   let outputkanji: string[] = []; // 型アノテーションを追加
@@ -23,57 +25,19 @@ function randomnum() {
 
 function App() {
   return (
-    <div className=" mt-16">
-      <h1 className="text-center text-sky-700 select-none">
-        四字熟語を作るアプリ
-      </h1>
-      <div className="mt-20">
-        <p className="text-7xl text-center font-yujisyuku" id="words">
-          〇〇〇〇
-        </p>
+    <div>
+      <div className=" text-right mr-16 mt-4">
+        <Menu />
       </div>
-      <div className=" text-center mt-24 select-none">
-        <button
-          className="rounded-xl p-2 border-2 text-2xl bg-sky-700/50 border-sky-700"
-          onClick={randomnum}
-        >
-          四字熟語を作る
-        </button>
-      </div>
-
-      <div className="mt-28 lg:mt-16 text-center border-2 border-sky-700 m-16 p-5 lg:border-0 lg:m-0 lg:p-0 select-none">
-        <h3>制作者情報</h3>
-        <div className=" justify-items-center grid gap-2 grid-cols-1 lg:grid-cols-3 mt-5">
-          <div className=" flex">
-            <p>sns：</p>
-            <a
-              href="https://x.com/hiitech2024"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ひー@情報系学生
-            </a>
-          </div>
-          <div className=" flex">
-            <p>website：</p>
-            <a
-              href="https://hii-site.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ひーサイト
-            </a>
-          </div>
-          <div className=" flex">
-            <p>Zenn：</p>
-            <a
-              href="https://zenn.dev/aputech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ひー@情報系学生
-            </a>
-          </div>
+      <div className=" mt-16">
+        <h1 className="text-center text-sky-700 select-none font-yujisyuku">
+          四字熟語を作るアプリ
+        </h1>
+        <div className="mt-20">
+          <p className="text-7xl text-center font-yujisyuku" id="words">
+            〇〇〇〇
+          </p>
+          <Button />
         </div>
       </div>
     </div>
